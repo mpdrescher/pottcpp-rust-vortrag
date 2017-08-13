74ef0bc3843b7d5efd89716fd19a139d07cb8792
Philipp Rosendahl <rosendahl@otris.de>
2017-08-13 17:52:21 +0200

    implemented pmap2 properly

--- src/pmap2.rs
+++ src/pmap2.rs
@@ -12,12 +12,14 @@ use std::thread::JoinHandle;
 use std::iter::Map;
 use std::slice::Chunks;
 use std::sync::Arc;
+use std::boxed::Box;
+use std::sync::Mutex;
 
 const THREADS: usize = 4;
 
 fn main() {
-    let data = generate_data(SETLEN/10);
-    println!("generiere {} testdaten", SETLEN/10);
+    let data = generate_data(SETLEN);
+    println!("generiere {} testdaten", SETLEN);
     let earlier = SystemTime::now();
     let _new_data = map_data(data);
     let dur = match SystemTime::now().duration_since(earlier) {
@@ -31,22 +33,16 @@ fn main() {
 }
 
 fn map_data(data: Vec<f64>) -> Vec<f64> {
-    let chunklen = data.len() / THREADS + 1;
-    data.chunks(chunklen)
-        .map(|x: Chunks<&[f64]> | -> JoinHandle<Map<&[f64], Fn(&f64) -> f64>>{ //über slices
-            //für jeden slice einen thread spawnen, der über den slice iteriert, und newton darauf anwendet.
-            thread::spawn(move || x.map(|y| {
-                newton(*y, NEWTON_ITER)
-            })) //Rückgabewert: ThreadHandle<Map<f64>>
-        })
-        .map(|handle| handle.join().expect("failed to join"))
-        .flat_map(|x| x)
-        .collect()
+    let mut handles:Vec<JoinHandle<Vec<f64>>> = Vec::new();
+    let mut data: Vec<Arc<_>> = data.chunks(data.len()/THREADS + 1).
+                                        map(|x| Arc::new(x.to_owned())).collect();
+    println!("{}", data.len());
+    for d in data {
+        handles.push(thread::spawn(move || d.iter().map(|x| newton(*x, NEWTON_ITER)).collect()));
+    }
+    let mut ret = Vec::with_capacity(THREADS);
+    for handle in handles {
+        ret.push(handle.join().unwrap());
+    }
+    ret.into_iter().flat_map(|x| x).collect()
 }
-
-/*fn map_data(data: Vec<f64>) -> Vec<f64> {
-    let chunklen = data.len() / THREADS + 1;
-    let chunks: Chunks<f64> = data.chunks(chunklen);
-    chunks.map(|x: &[f64]| x); 
-    vec!()
-}*/
