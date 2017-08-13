66ec2cbedc4db1bc32f7a1744d2a7f84993588e9
Matthias Drescher <matthias_p_drescher@t-online.de>
2017-08-13 18:49:47 +0200

    Format pmap2

--- src/pmap2.rs
+++ src/pmap2.rs
@@ -9,11 +9,7 @@ use testdata::{
 use std::time::SystemTime;
 use std::thread;
 use std::thread::JoinHandle;
-use std::iter::Map;
-use std::slice::Chunks;
 use std::sync::Arc;
-use std::boxed::Box;
-use std::sync::Mutex;
 
 const THREADS: usize = 4;
 
@@ -33,11 +29,11 @@ fn main() {
 }
 
 fn map_data(data: Vec<f64>) -> Vec<f64> {
-    let mut handles:Vec<JoinHandle<Vec<f64>>> = Vec::new();
-    let mut data = data.chunks(data.len()/THREADS + 1).
-                                        map(|x| Arc::new(x.to_owned()));
-    for d in data {
-        handles.push(thread::spawn(move || d.iter().map(|x| newton(*x, NEWTON_ITER)).collect()));
+    let mut handles: Vec<JoinHandle<Vec<f64>>> = Vec::new();
+    let chunk_len = data.len()/THREADS + 1;
+    let chunks = data.chunks(chunk_len).map(|x| Arc::new(x.to_owned()));
+    for chunk in chunks {
+        handles.push(thread::spawn(move || chunk.iter().map(|x| newton(*x, NEWTON_ITER)).collect()));
     }
     handles.into_iter().flat_map(|x| x.join().unwrap()).collect()
 }
